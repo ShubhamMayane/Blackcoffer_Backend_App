989f1b27e1f41daf54cff9e1b8f9b1746d9cdd37
Shubham Mayane <52218928+ShubhamMayane@users.noreply.github.com>
2024-08-09 18:44:35 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
 
 
 const app=express();
 
 
-
+app.use(bodyParser.urlencoded({extended:true}));
 
 
 
@@ -47,7 +49,7 @@ const port =process.env.PORT || 5000;
 
 app.listen(port,function(){
 
-    console.log("Server is running at port 5000");
+    console.log("Server is running at port "+port);
     
 });
 
