57be70fbfaa521efa820e124e14af7470461dd0a
Shubham Mayane <52218928+ShubhamMayane@users.noreply.github.com>
2024-08-09 18:17:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 import express from "express";
-
-import myCollectionModel from "./databaelogic.js";
+import mongoose from "mongoose";
 
 
 const app=express();
+
+
+import mongoose from "mongoose";
+
+
+
+//establishing a connection to mongodb database
+mongoose.connect("mongodb+srv://ShubhamSm009:admin@cluster0.ohtho.mongodb.net/MainDatabase");
+
+
+const objectSchema=new mongoose.Schema({
+            end_year:String,
+            intensity: Number,
+            sector: String,
+            topic: String,
+            insight:String,
+            url: String,
+            region: String,
+            start_year: String,
+            impact: String,
+            added: String,
+            published: String,
+            country: String,
+            relevance: Number,
+            pestle: String,
+            source: String,
+            title: String,
+            likelihood: Number
+});
+
+
+const myCollectionModel=mongoose.model("MainCollection",objectSchema);
+
+
+
+
 
 
 const port =process.env.PORT || 5000;
